572706657a080473281647eb2b4e5e8746faac02
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-21 23:03:41 -0400

    first commit. Built UI and attached IBOutlets

---
+++ Scribe/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Justin Lindsay on 7/21/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
