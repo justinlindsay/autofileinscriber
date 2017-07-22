3da58235e28f1dc67e7b862f26dc5431fd9e1e0c
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-21 23:26:39 -0400

    Implimented Speech Recognizer.

--- Scribe/CircleButton.swift
+++ Scribe/CircleButton.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
+@IBDesignable
 class CircleButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var cornerRadius: CGFloat = 30.0 {
+        
+        didSet{
+            
+            setUpView()
+            
+        }
+        
     }
-    */
-
+    
+    override func prepareForInterfaceBuilder() {
+        
+        setUpView()
+        
+    }
+    
+    func setUpView() {
+        
+        layer.cornerRadius = cornerRadius
+        
+    }
+    
+    
 }
--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var transcriptionTxtField: UITextView!
+    
+    
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        activitySpinner.isHidden = true
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func requestSpeechAuth() {
+        
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                
+                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                    
+                    do {
+                        
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        sound.play()
+                        
+                    } catch {
+                        
+                        print("Error")
+                        
+                    }
+                    
+                    
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                     
+                        if let error = error {
+                            
+                            print("There was an error: \(error)")
+                            
+                        } else {
+                            
+                            print(result?.bestTranscription.formattedString as Any)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+        }
     }
-
+    
+    
+    @IBAction func playBtnPressed(_ sender: CircleButton) {
+        
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
+        
+    }
+    
 
 }
 
